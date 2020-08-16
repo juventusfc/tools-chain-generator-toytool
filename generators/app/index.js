e9ab210c45fd5a2e7e3f65b5af6ee9f86baab3a1
FrankHu <frank.hu@perficient.com>
2020-08-16 21:59:54 +0800

    test

--- generators/app/index.js
+++ generators/app/index.js
@@ -11,9 +11,34 @@ module.exports = class extends Generator {
   }
 
   creating() {
+    // generate package.json
     this.fs.copyTpl(
       this.templatePath("package.json"),
-      this.destinationPath("package.json")
+      this.destinationPath("package.json"),
+      { title: "Templating with Yeoman" }
+    );
+    this.npmInstall(
+      [
+        "webpack",
+        "webpack-cli",
+        "webpack-dev-server",
+        "babel-loader",
+        "@babel/core",
+        "@babel/preset-env",
+        "@babel/plugin-transform-react-jsx",
+        "mocha",
+        "@babel/register",
+        "nyc",
+      ],
+      {
+        "save-dev": true,
+      }
+    );
+
+    // generate lib
+    this.fs.copyTpl(
+      this.templatePath("lib/sum"),
+      this.destinationPath("lib/sum")
     );
     // this.fs.copyTpl(
     //   this.templatePath("index.html"),
